a0f495f03d5a02f333775206fef3706aa96b8d82
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-07-04 23:05:34 +0200

    feat: Added adapted database to be postgres in production

--- learou/settings/production.py
+++ learou/settings/production.py
@@ -6,3 +6,15 @@ try:
     from .base import *
 except ImportError:
     pass
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
